1177b8e14dbf0486a9c40df8411e158c027a10de
Doug Penny <dpenny52@gmail.com>
2017-02-04 18:25:36 -0800

    some tests

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "test new todo saves properly" do
+    todo = Todo.new(title: "hello")
+    assert todo.save
+  end
+
+  test "get a todo works" do
+    todo = Todo.find(2)
+    assert todo.title == "OH MY GAWD"
+    assert_not todo.completed
+  end
+
+  test "get missing todo raises exception" do
+    assert_raises(Exception) {
+      todo = Todo.find(3)
+    }
+  end
+
+  test "save todo without title raises exception" do
+    todo = Todo.new()
+    assert_raises(Exception) {
+      todo.save
+    }
+  end
 end
